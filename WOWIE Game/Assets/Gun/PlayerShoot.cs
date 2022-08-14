ade1b247089095871c35d682c51a9eb9bdc4429c
dkaloger <dimitriskalogeropoulos3@gmail.com>
2022-08-14 17:11:52 +0300

    save

--- WOWIE Game/Assets/Gun/PlayerShoot.cs
+++ WOWIE Game/Assets/Gun/PlayerShoot.cs
@@ -27,6 +27,7 @@ public class PlayerShoot : MonoBehaviour, IHeldItem
         Playeranim.SetBool("HoldingGun", true);
 
         if (Input.GetButton("Fire1"))
-            bullets.Spawn(transform.position, transform.right);
+          //  transform.position + (Vector3)movement.normalized + new Vector3(0.0f, -0.5f, 0f)
+            bullets.Spawn(Playeranim.gameObject.transform.position +  new Vector3(0.0f, -0.3f, 0f), (Vector3)Playeranim.gameObject.GetComponent<PlayerController>().movement.normalized);
     }
 }
