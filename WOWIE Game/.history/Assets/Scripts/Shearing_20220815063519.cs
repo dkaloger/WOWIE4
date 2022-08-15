ec074f572ac5bf489dba29fbdcc31878840f5a4a
Alexander Asay <alex.asay@sbcglobal.net>
2022-08-15 08:00:27 -0700

    Done with Sheep

---
+++ WOWIE Game/.history/Assets/Scripts/Shearing_20220815063519.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shearing : MonoBehaviour
+{
+    [SerializeField] private Sprite shearedsheep;
+    [SerializeField] private Sprite fullSheep;
+    [SerializeField] private GameObject wool;
+    
+    private float t;
+    private float cooldown;
+    private bool startTimer = false;
+    public bool dead = false;
+    private bool seen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Ore") && gameObject.GetComponent<SpriteRenderer>().sprite != shearedsheep && !dead)
+        {
+            collision.transform.parent.parent.GetComponent<PlayerController>().Helditem = null;
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = shearedsheep;
+            Instantiate(wool, new Vector2(collision.transform.parent.parent.GetComponent<Transform>().position.x, collision.transform.parent.parent.GetComponent<Transform>().position.y),Quaternion.identity);
+            startTimer = true;
+        }
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(dead){
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            gameObject.GetComponent<SpriteRenderer>().sprite = shearedsheep;
+            gameObject.tag = "Holdable";
+        }
+        if(startTimer){
+            t+=Time.deltaTime*1f;
+            if(t >= 60){
+                t = 0;
+                gameObject.GetComponent<SpriteRenderer>().sprite = fullSheep;
+                startTimer = false;
+            }
+        }
+
+        int i = -1;
+        if (transform.position.x < 15.4)
+        {
+            i = 0;
+        }
+        else if (transform.position.x < 60.3)
+        {
+            i = 1;
+        }
+        else if (transform.position.x < 85)
+        {
+            i = 2;
+        }
+        Vector2 screenPosition = GetComponent<Camera>().WorldToScreenPoint(transform.position);
+        if (!(screenPosition.y > Screen.height || screenPosition.y < 0 || screenPosition.x > Screen.width || screenPosition.x < 0))
+        {
+            seen = true;
+            Debug.Log("Seen!");
+        }
+        else
+        {
+            if (seen)
+            {
+                Destroy(gameObject);
+                SheepSpawner.indivCount[i]++;
+                seen = false;
+            }
+        }
+        if(SheepSpawner.indivCount[i] == 4){
+            SheepSpawner.indivCount[i] = 0;
+            SheepSpawner.zonesCount[i]--;
+        }
+        
+    }
+}
---
+++ WOWIE Game/.history/Assets/Scripts/Shearing_20220815072226.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shearing : MonoBehaviour
+{
+    [SerializeField] private Sprite shearedsheep;
+    [SerializeField] private Sprite fullSheep;
+    [SerializeField] private GameObject wool;
+    [SerializeField] private GameObject sheep;
+    private float t = 0;
+    private float cooldown;
+    private bool startTimer = false;
+    public bool dead = false;
+    private float t2 = 0;
+    private Vector2 savedPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Ore") && gameObject.GetComponent<SpriteRenderer>().sprite != shearedsheep && !dead)
+        {
+            collision.transform.parent.parent.GetComponent<PlayerController>().Helditem = null;
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = shearedsheep;
+            Instantiate(wool, new Vector2(collision.transform.parent.parent.GetComponent<Transform>().position.x, collision.transform.parent.parent.GetComponent<Transform>().position.y),Quaternion.identity);
+            startTimer = true;
+        }
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(dead){
+            if(t2 == 0){
+                GetComponent<BoxCollider2D>().isTrigger = true;
+                gameObject.GetComponent<SpriteRenderer>().sprite = shearedsheep;
+                gameObject.tag = "Holdable";
+                savedPos = new Vector2(transform.position.x, transform.position.y);
+            }
+            t2 += Time.deltaTime*1f;
+            if(t2 >= 5){
+                GameObject newsheep = Instantiate(sheep,savedPos, Quaternion.identity);
+                newsheep.GetComponent<Shearing>().dead = false;
+                newsheep.GetComponent<SpriteRenderer>().sprite = fullSheep;
+                newsheep.GetComponent<SpriteRenderer>().enabled = true;
+                newsheep.tag = "Untagged";
+                newsheep.Get
+                Destroy(gameObject);
+                t2 = 0;
+                dead = false;
+            }
+        }
+        if(startTimer){
+            t+=Time.deltaTime*1f;
+            if(t >= 60){
+                t = 0;
+                gameObject.GetComponent<SpriteRenderer>().sprite = fullSheep;
+                startTimer = false;
+            }
+        }
+        
+    }
+}
